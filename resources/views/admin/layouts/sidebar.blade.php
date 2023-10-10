2a1067a69101674ec62ad90837c7ddffa2924d5d
nguyenanbinh <nguyenanbinh.98.dn@gmail.com>
2023-10-10 13:06:34 +0700

    brand feature

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -36,6 +36,20 @@
             </li>
             {{--End Manage Categories--}}
 
+            {{--Start Products--}}
+            <li
+                class="dropdown {{ setActive([
+                    'admin.brand.*',
+                ]) }}">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-box"></i>
+                    <span>Manage Products</span></a>
+                <ul class="dropdown-menu">
+                    <li class="{{ setActive(['admin.brand.*']) }}">
+                        <a class="nav-link" href="{{ route('admin.brand.index') }}">Brands</a></li>
+                </ul>
+            </li>
+            {{--End Products--}}
+
             {{--Manage Website--}}
             <li
                 class="dropdown {{ setActive([
--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\ChildCategoryController;
 use App\Http\Controllers\Backend\ProfileController;
@@ -31,4 +32,7 @@ Route::put('child-category/change-status', [ChildCategoryController::class, 'cha
 Route::resource('child-category', ChildCategoryController::class)->except('show');
 Route::get('get-subcategories', [ChildCategoryController::class, 'getSubCategories'])->name('get-subcategories');
 
+/** Brand routes */
+Route::put('brand/change-status', [BrandController::class, 'changeStatus'])->name('brand.change-status');
+Route::resource('brand', BrandController::class)->except('show');
 
